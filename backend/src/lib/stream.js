d3e248e550a309cd26f5bf6131dbecb033eb85a2
Rohit Bhatt <bhattrohed@gmail.com>
2025-10-22 21:25:12 +0530

    fixed some issues

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -1,29 +1,30 @@
-import {StreamChat} from "stream-chat";
+import { StreamChat } from "stream-chat";
 import "dotenv/config";
 
 const apiKey = process.env.STEAM_API_KEY;
 const apiSecret = process.env.STEAM_API_SECRET;
 
-if(!apiKey || !apiSecret){
-    console.log("Stream api key or secret is missing");
+if (!apiKey || !apiSecret) {
+  console.error("Stream API key or Secret is missing");
 }
 
 const streamClient = StreamChat.getInstance(apiKey, apiSecret);
 
 export const upsertStreamUser = async (userData) => {
-    try {
-        await streamClient.upsertUser(userData)
-        return userData;
-    } catch (error) {
-        console.log("Error upserting Stream User:",error);
-    }
-}
+  try {
+    await streamClient.upsertUsers([userData]);
+    return userData;
+  } catch (error) {
+    console.error("Error upserting Stream user:", error);
+  }
+};
 
 export const generateStreamToken = (userId) => {
-    try {
-        const userIdStr = userId.toString();
-        return streamClient.createToken(userIdStr);
-    } catch (error) {
-        console.error("error generating stream token:", error);
-    }
-}
+  try {
+    // ensure userId is a string
+    const userIdStr = userId.toString();
+    return streamClient.createToken(userIdStr);
+  } catch (error) {
+    console.error("Error generating Stream token:", error);
+  }
+};
--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,66 +1,72 @@
-import mongoose from "mongoose"
-import bcrypt from "bcryptjs"
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
-const userSchema = mongoose.Schema({
-    fullName:{
-        type:String,
-        required: true,
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
     },
-    email:{
-        type:String,
-        required: true,
-        unique:true,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    password:{
-        type:String,
-        required:true,
-        minlength:6,
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
     },
-    bio:{
-        type:String,
-        default:"",
+    bio: {
+      type: String,
+      default: "",
     },
     profilePic: {
-        type:String,
-        default:"",
+      type: String,
+      default: "",
     },
     nativeLanguage: {
-        type: String,
-        default: "",
+      type: String,
+      default: "",
     },
     learningLanguage: {
-        type: String,
-        default: ""
-    }, 
+      type: String,
+      default: "",
+    },
     location: {
-        type: String,
-        default: "",
+      type: String,
+      default: "",
     },
     isOnboarded: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false,
     },
-    friends: [{
+    friends: [
+      {
         type: mongoose.Schema.Types.ObjectId,
-        ref:"User",
-    }]
-}, {timestamps: true});
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
-userSchema.pre("save", async function(next){
-    if(!this.isModified("password")) return next();
-    try{
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-        next()
-    } catch(error){
-        next(error);
-    }
-})
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
 
-userSchema.methods.matchPassword = async function(enteredPassword){
-    const isPasswordCorrect = await bcrypt.compare(enteredPassword, this.password);
-    return isPasswordCorrect;
-}
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  const isPasswordCorrect = await bcrypt.compare(enteredPassword, this.password);
+  return isPasswordCorrect;
+};
 
 const User = mongoose.model("User", userSchema);
 
